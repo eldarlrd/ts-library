2158bc398825f01cb44da5c0779a4a81c42d45f4
eldarlrd <eldarlrd@pm.me>
2023-07-16 15:49:36 +0400

    feat: Footer

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import m from 'mithril';
 import 'virtual:uno.css';
+import githubLogo from '@/assets/github.svg';
 
-export const App = () => {
+const githubURL: string = 'https://github.com/eldarlrd';
+
+interface VirtualDOM {
+  view: () => m.Vnode;
+}
+
+export const App = (): VirtualDOM => {
   return {
     view: () =>
       m('div#app', [
@@ -9,8 +16,25 @@ export const App = () => {
           'header',
           m('h1', { class: 'prose prose-sky' }, 'TypeScript Library')
         ),
+
         m('main', { class: 'flex items-center justify-center' }, [
           m('h2', 'Mithril')
+        ]),
+
+        m('footer', [
+          m('p', {
+            class: 'm-6 flex select-none flex-col items-center justify-center text-center text-lg font-bold text-black drop-shadow-xl sm:text-xl md:text-2xl'
+          }, ['by ', m('a', {
+            class: 'flex items-center justify-center gap-1 text-center transition-transform hover:scale-110flex items-center justify-center gap-1 text-center transition-transform hover:scale-110',
+            title: 'Go to GitHub',
+            target: '_blank',
+            rel: 'noreferrer',
+            href: githubURL
+        }, [m('img', {
+          class: 'h-6 w-6',
+          alt: 'GitHub Logo',
+          src: githubLogo
+        }), 'eldarlrd'])])
         ])
       ])
   };
